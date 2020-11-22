d555ef59fba5208c7903885624c990dbbda5de1d
bharat <bharatchoithani10@gmail.com>
2020-11-22 11:55:51 +0530

    new post

--- blog_app/lib/pages/newpost.dart
+++ blog_app/lib/pages/newpost.dart
@@ -1,4 +1,6 @@
+import 'dart:io';
 import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
 
 class NewPost extends StatefulWidget {
   @override
@@ -6,11 +8,194 @@ class NewPost extends StatefulWidget {
 }
 
 class _NewPostState extends State<NewPost> {
+
+  File imageFile;
+
+  _openGallery() async {
+    PickedFile pickedFile = await ImagePicker().getImage(source: ImageSource.gallery);
+    var picture = File(pickedFile.path);
+    this.setState(() {
+      imageFile = picture;
+    });
+  }
+  _openCamera() async {
+    PickedFile pickedFile = await ImagePicker().getImage(source: ImageSource.camera);
+    var picture = File(pickedFile.path);
+    this.setState(() {
+      imageFile = picture;
+    });
+  }
+
+  Future<void> _showChoiceDialog(BuildContext context) {
+    return showDialog(context: context, builder: (BuildContext context){
+      return AlertDialog(
+        title: Text(
+          'Make a Choice!',
+        ),
+        content: SingleChildScrollView(
+          child: ListBody(
+            children: <Widget>[
+              GestureDetector(
+                child: Text(
+                  'Gallery'
+                ),
+                onTap: () {
+                  _openGallery();
+                },
+              ),
+              Padding(
+                padding: EdgeInsets.all(8.0),
+              ),
+              GestureDetector(
+                child: Text(
+                    'Camera'
+                ),
+                onTap: () {
+                  _openCamera();
+                },
+              ),
+            ],
+          ),
+        ),
+      );
+    });
+  }
+
+  final _formKey = GlobalKey<FormState>();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('New'),
+      resizeToAvoidBottomPadding: false,
+      body: Container(
+        child: Form(
+          child: Column(
+            key: _formKey,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              Container(
+                child: Stack(
+                  children: <Widget>[
+                    Container(
+                      padding: EdgeInsets.fromLTRB(10.0, 10.0, 0.0, 0.0),
+                      child: Text(
+                        'New Post',
+                        style: TextStyle(
+                          fontSize: 40.0,
+                          fontWeight: FontWeight.bold,
+                          fontFamily: 'Montserrat',
+                        ),
+                      ),
+                    ),
+                    Container(
+                      padding: EdgeInsets.fromLTRB(212.0, 10.0, 0.0, 0.0),
+                      child: Text(
+                        '.',
+                        style: TextStyle(
+                          fontSize: 40.0,
+                          fontWeight: FontWeight.bold,
+                          fontFamily: 'Montserrat',
+                          color: Colors.lightBlue
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              Container(
+                padding: EdgeInsets.only(top: 45.0, left: 20.0, right: 20.0),
+                child: Column(
+                  children: <Widget>[
+                    TextFormField(
+                      decoration: InputDecoration(
+                          labelText: 'TITLE',
+                          labelStyle: TextStyle(
+                            fontFamily: 'Montserrat',
+                            fontWeight: FontWeight.bold,
+                            color: Colors.grey,
+                          ),
+                          focusedBorder: UnderlineInputBorder(
+                              borderSide: BorderSide(color: Colors.lightBlue
+                              )
+                          )
+                      ),
+                    ),
+                    SizedBox(
+                      height: 20.0,
+                    ),
+                    TextFormField(
+                      decoration: InputDecoration(
+                          labelText: 'CONTENT',
+                          labelStyle: TextStyle(
+                            fontFamily: 'Montserrat',
+                            fontWeight: FontWeight.bold,
+                            color: Colors.grey,
+                          ),
+                          focusedBorder: UnderlineInputBorder(
+                              borderSide: BorderSide(color: Colors.lightBlue)
+                          )
+                      ),
+                    ),
+                    SizedBox(
+                      height: 30.0,
+                    ),
+                    InkWell(
+                      onTap: () {
+                        _showChoiceDialog(context);
+                      },
+                      child: Container(
+                        width: 160.0,
+                        height: 50.0,
+                        decoration: BoxDecoration(
+                          color: Colors.grey,
+                          border: Border.all(color: Colors.white, width: 2.0),
+                          borderRadius: BorderRadius.circular(10.0),
+                        ),
+                        child: Center(
+                          child: Text(
+                            'Select Image',
+                            style: TextStyle(
+                              fontSize: 18.0,
+                              color: Colors.white,
+                              fontWeight: FontWeight.bold,
+                              fontFamily: 'Montserrat',
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
+                    SizedBox(
+                      height: 40.0,
+                    ),
+                    InkWell(
+                      onTap: () {},
+                      child: Container(
+                        width: 120.0,
+                        height: 50.0,
+                        decoration: BoxDecoration(
+                          color: Colors.lightBlueAccent,
+                          border: Border.all(color: Colors.white, width: 2.0),
+                          borderRadius: BorderRadius.circular(20.0),
+                        ),
+                        child: Center(
+                          child: Text(
+                            'UPLOAD',
+                            style: TextStyle(
+                                fontSize: 18.0,
+                                color: Colors.white,
+                                fontWeight: FontWeight.bold,
+                              fontFamily: 'Montserrat',
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ),
       ),
     )
     ;
